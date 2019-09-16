11ce47ea8730931a6c40d76ed321335ce9232ad8
Aporoid <Aporoid@hotmail.com>
2019-09-15 20:08:42 -0500

    Stable-ish version of travel

--- Phase Shift/Assets/Scripts/PhaseTest2.cs
+++ Phase Shift/Assets/Scripts/PhaseTest2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhaseTest2 : MonoBehaviour
 {
@@ -21,33 +22,71 @@ public class PhaseTest2 : MonoBehaviour
 	[SerializeField]
 	private bool backToDimension2;
 
+	[SerializeField]
+	private Text interactText;
+	[SerializeField]
+	private Image flashImage;
+
+	[SerializeField]
+	private AudioClip teleport;
+
+	private new AudioSource audio;
+
+	private void Start()
+	{
+		audio = GetComponent<AudioSource>();
+		flashImage.enabled = false;
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
-		if (toDimension1 == true)
+		interactText.text = "[ press F to Dimensional Travel ]";
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		interactText.text = "";
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (Input.GetButtonDown("Interact"))
 		{
-			if(backToDimension2 == true)
+			StartCoroutine(Countdown());
+			if (toDimension1 == true)
 			{
-				player2Controller.SetActive(false);
-				player2BController.SetActive(true);
+				if(backToDimension2 == true)
+				{
+					player2Controller.SetActive(false);
+					player2BController.SetActive(true);
+				}
+				else
+				{
+					player1BController.SetActive(false);
+					player2Controller.SetActive(true);
+				}
 			}
-			else
+			else if (toDimension2 == true)
 			{
-				player1BController.SetActive(false);
-				player2Controller.SetActive(true);
+				if (backToDimension1 == true)
+				{
+					player1Controller.SetActive(false);
+					player1BController.SetActive(true);
+				}
+				else
+				{
+					player1Controller.SetActive(false);
+					player1BController.SetActive(true);
+				}
 			}
 		}
-		else if (toDimension2 == true)
-		{
-			if (backToDimension1 == true)
-			{
-				player1Controller.SetActive(false);
-				player1BController.SetActive(true);
-			}
-			else
-			{
-				player1Controller.SetActive(false);
-				player1BController.SetActive(true);
-			}
-		}
+	}
+
+	private IEnumerator Countdown()
+	{
+		flashImage.enabled = true;
+		audio.PlayOneShot(teleport, 1);
+		yield return new WaitForSeconds(0.1f);
+		flashImage.enabled = false;
 	}
 }
