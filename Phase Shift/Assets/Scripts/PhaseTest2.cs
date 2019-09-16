8c8d36f0899cd87709cbf947b7235bb64e91627c
Aporoid <Aporoid@hotmail.com>
2019-09-15 21:25:39 -0500

    Most stable setup
    Not perfect by any means.

--- Phase Shift/Assets/Scripts/PhaseTest2.cs
+++ Phase Shift/Assets/Scripts/PhaseTest2.cs
@@ -5,14 +5,23 @@ using UnityEngine.UI;
 
 public class PhaseTest2 : MonoBehaviour
 {
+	#region serialized fields
+	[Tooltip("The controller on the closest ledge to spawn.")]
 	[SerializeField]
 	private GameObject player1Controller;
+
+	[Tooltip("The controller opposite 1, near the green door.")]
 	[SerializeField]
 	private GameObject player2Controller;
+
+	[Tooltip("The controller in dimension B, closest to the Blue wall.")]
 	[SerializeField]
 	private GameObject player1BController;
+
+	[Tooltip("The controller in dimension B, closest to the Orange wall. Used to re-navigate Dimension B in reverse.")]
 	[SerializeField]
 	private GameObject player2BController;
+
 	[SerializeField]
 	private bool toDimension1;
 	[SerializeField]
@@ -29,6 +38,7 @@ public class PhaseTest2 : MonoBehaviour
 
 	[SerializeField]
 	private AudioClip teleport;
+	#endregion
 
 	private new AudioSource audio;
 
@@ -36,6 +46,11 @@ public class PhaseTest2 : MonoBehaviour
 	{
 		audio = GetComponent<AudioSource>();
 		flashImage.enabled = false;
+	}
+
+	private void Update()
+	{
+		CheckPlayerEyes();
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -55,12 +70,12 @@ public class PhaseTest2 : MonoBehaviour
 			StartCoroutine(Countdown());
 			if (toDimension1 == true)
 			{
-				if(backToDimension2 == true)
+				if (backToDimension1 == true)
 				{
-					player2Controller.SetActive(false);
-					player2BController.SetActive(true);
+					player1BController.SetActive(false);
+					player1Controller.SetActive(true);
 				}
-				else
+				else if (backToDimension1 == false)
 				{
 					player1BController.SetActive(false);
 					player2Controller.SetActive(true);
@@ -68,12 +83,13 @@ public class PhaseTest2 : MonoBehaviour
 			}
 			else if (toDimension2 == true)
 			{
-				if (backToDimension1 == true)
+				if (backToDimension2 == true)
 				{
 					player1Controller.SetActive(false);
-					player1BController.SetActive(true);
+					player2Controller.SetActive(false);
+					player2BController.SetActive(true);
 				}
-				else
+				else if (backToDimension2 == false)
 				{
 					player1Controller.SetActive(false);
 					player1BController.SetActive(true);
@@ -82,11 +98,32 @@ public class PhaseTest2 : MonoBehaviour
 		}
 	}
 
-	private IEnumerator Countdown()
+	private IEnumerator Countdown() //used to create the dramatic flash when teleporting
 	{
 		flashImage.enabled = true;
 		audio.PlayOneShot(teleport, 1);
 		yield return new WaitForSeconds(0.1f);
+		interactText.text = "";
 		flashImage.enabled = false;
+	}
+
+	private void CheckPlayerEyes() //simply used for debugging purposes
+	{
+		if(player1Controller == true)
+		{
+			Debug.Log("Player 1 eyes");
+		}
+		else if (player1BController == true)
+		{
+			Debug.Log("Player 1B eyes");
+		}
+		else if (player2Controller == true)
+		{
+			Debug.Log("Player 2 eyes");
+		}
+		else if (player2BController == true)
+		{
+			Debug.Log("Player 2B eyes");
+		}
 	}
 }
